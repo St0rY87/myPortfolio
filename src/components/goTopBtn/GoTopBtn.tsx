80285c32c97bec53ad1eb6174bc16a4cfd9c1746
Dmitriy <mr.dima337@gmail.com>
2026-03-30 11:26:16 +0300

    refactor: refactor style bnt-go-top

--- src/components/goTopBtn/GoTopBtn.tsx
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -19,16 +19,14 @@ export const GoTopBtn = () => {
     };
   }, []);
 
-
-
   return (
-      <StyledGoTopBtn onClick={() => scroll.scrollToTop()} $isShowBtn={isShowBtn}>
-        <Icon iconId="arrowGoTop" width="30" height="30" viewBox="0 0 24 24" />
-      </StyledGoTopBtn>
+    <StyledGoTopBtn onClick={() => scroll.scrollToTop()} $isShowBtn={isShowBtn}>
+      <Icon iconId="arrowGoTop" width="30" height="30" viewBox="0 0 24 24" />
+    </StyledGoTopBtn>
   );
 };
 
-const StyledGoTopBtn = styled.button<{$isShowBtn: boolean}>`
+const StyledGoTopBtn = styled.button<{ $isShowBtn: boolean }>`
   position: fixed;
   z-index: 100;
   bottom: 30px;
@@ -37,16 +35,25 @@ const StyledGoTopBtn = styled.button<{$isShowBtn: boolean}>`
   aspect-ratio: 1;
   border-radius: 15px;
   background-color: ${theme.colors.accentColor};
-  opacity: ${props => [props.$isShowBtn ? 1 : 0]};
-  pointer-events: ${props => [props.$isShowBtn ? "auto" : "none"]};
+  opacity: ${(props) => [props.$isShowBtn ? 1 : 0]};
+  pointer-events: ${(props) => [props.$isShowBtn ? "auto" : "none"]};
   transition: 0.4s ease-in-out;
   &:hover {
     opacity: 0.8;
   }
-
   svg {
     fill: #fff;
   }
 
+  @media ${theme.media.tablet} {
+    bottom: 20px;
+    right: 20px;
+    width: 40px;
+    border-radius: 10px;
+  }
 
+  @media ${theme.media.mobile} {
+    bottom: 10px;
+    right: 10px;
+  }
 `;
