8e23d171011022714fe6335b3405cbb04a737c00
Dmitriy <mr.dima337@gmail.com>
2026-03-31 16:06:04 +0300

    refactor: add prefers-reduced-motion: reduce

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -39,7 +39,15 @@ export const GlobalStyles = createGlobalStyle`
     background: ${theme.colors.primaryBg};
   }
 
-
-
+  @media (prefers-reduced-motion: reduce) {
+  *,
+  *::before,
+  *::after {
+    animation-duration: 0.01ms !important;
+    animation-iteration-count: 1 !important;
+    transition-duration: 0.01ms !important;
+    scroll-behavior: auto !important;
+  }
+}
 
  `;
