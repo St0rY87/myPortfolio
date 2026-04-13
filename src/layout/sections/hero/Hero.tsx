400f012d21e1a727964af7fbd3ef81c85fe4cbfe
Dmitriy <mr.dima337@gmail.com>
2026-04-13 23:23:49 +0300

    refactor: removed unused imports

--- src/layout/sections/hero/Hero.tsx
+++ src/layout/sections/hero/Hero.tsx
@@ -6,7 +6,7 @@ import { theme } from "../../../styles/Theme";
 import Typewriter from "typewriter-effect";
 import { Link } from "react-scroll";
 import photo from "../../../assets/images/hero.png";
-import { Parallax } from "../../../components/parallax/Parallax";
+// import { Parallax } from "../../../components/parallax/Parallax";
 
 const tickerTape = [
   "PROBLEM SOLVING",
