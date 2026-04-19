7ef068e7049a930e9b028d6ba8883f50ba2cc266
Dmitriy <mr.dima337@gmail.com>
2026-04-19 11:23:16 +0300

    refactor: refactor scrollTo top of GoToTopBtn

--- src/components/goTopBtn/GoTopBtn.tsx
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Icon } from "../icon/Icon";
-import { animateScroll as scroll } from "react-scroll";
 
 export const GoTopBtn = () => {
   const [isShowBtn, setIsShowBtn] = useState(false);
@@ -19,7 +18,7 @@ export const GoTopBtn = () => {
   }, []);
 
   return (
-    <StyledGoTopBtn onClick={() => scroll.scrollToTop()} $isShowBtn={isShowBtn}>
+    <StyledGoTopBtn onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} $isShowBtn={isShowBtn}>
       <Icon iconId="arrowGoTop" width="30" height="30" viewBox="0 0 24 24" />
     </StyledGoTopBtn>
   );
