b54fd5a7775c9614b77621996417087d757f1e92
Dmitriy <mr.dima337@gmail.com>
2026-04-19 11:00:28 +0300

    refactor: refactor offset of react-scroll on mobile

--- src/layout/header/mobileMenu/mobileMenuItem/MobileMenuItem.tsx
+++ src/layout/header/mobileMenu/mobileMenuItem/MobileMenuItem.tsx
@@ -9,7 +9,7 @@ type MobileMenuItemProps = {
 export const MobileMenuItem = ({ item, showMenu }: MobileMenuItemProps) => {
   return (
     <ListItem>
-      <NavLink smooth={true}  offset={-63} onClick={showMenu} to={`${item.toLowerCase()}`}>
+      <NavLink smooth={true}  offset={-60} onClick={showMenu} to={`${item.toLowerCase()}`}>
         {item}
       </NavLink>
     </ListItem>
