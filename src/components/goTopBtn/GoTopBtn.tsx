77f7380edae03d90c8b0b5525ad3643bb79e93bd
Dmitriy <mr.dima337@gmail.com>
2026-04-19 12:19:06 +0300

    refactor: refactor scroll to top by using react-scroll

--- src/components/goTopBtn/GoTopBtn.tsx
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -1,31 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Icon } from "../icon/Icon";
-
-// export const GoTopBtn = () => {
-//   const [isShowBtn, setIsShowBtn] = useState(false);
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       setIsShowBtn(window.scrollY > 300);
-//     };
-
-//     window.addEventListener("scroll", handleScroll);
-
-//     return () => {
-//       window.removeEventListener("scroll", handleScroll);
-//     };
-//   }, []);
-
-//   return (
-//     <StyledGoTopBtn
-//       onClick={() => window.scrollTo({ top: -30, behavior: "smooth" })}
-//       $isShowBtn={isShowBtn}
-//     >
-//       <Icon iconId="arrowGoTop" width="30" height="30" viewBox="0 0 24 24" />
-//     </StyledGoTopBtn>
-//   );
-// };
+import { animateScroll as scroll } from "react-scroll";
 
 export const GoTopBtn = () => {
   const [isShowBtn, setIsShowBtn] = useState(false);
@@ -42,20 +18,8 @@ export const GoTopBtn = () => {
     };
   }, []);
 
-  const scrollToTop = () => {
-    const scrollElement = document.scrollingElement || document.documentElement;
-    
-    scrollElement.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  };
-
   return (
-    <StyledGoTopBtn
-      onClick={scrollToTop}
-      $isShowBtn={isShowBtn}
-    >
+    <StyledGoTopBtn onClick={() => scroll.scrollToTop()} $isShowBtn={isShowBtn}>
       <Icon iconId="arrowGoTop" width="30" height="30" viewBox="0 0 24 24" />
     </StyledGoTopBtn>
   );
@@ -71,29 +35,30 @@ const StyledGoTopBtn = styled.button<{ $isShowBtn: boolean }>`
   width: 50px;
   aspect-ratio: 1;
   border-radius: 15px;
-  background-color: ${({ theme }) => theme.colors.accentColor};
+  background-color: ${({theme}) => theme.colors.accentColor};
   opacity: ${(props) => [props.$isShowBtn ? 1 : 0]};
   pointer-events: ${(props) => [props.$isShowBtn ? "auto" : "none"]};
   transition: 0.3s ease-in-out;
   & svg {
-    fill: #fff;
+      fill: #fff;
   }
+  
 
-  @media ${({ theme }) => theme.media.tablet} {
+  @media ${({theme}) => theme.media.tablet} {
     bottom: 20px;
     right: 20px;
     width: 40px;
     border-radius: 10px;
   }
 
-  @media ${({ theme }) => theme.media.mobile} {
+  @media ${({theme}) => theme.media.mobile} {
     bottom: 10px;
     right: 10px;
   }
 
   @media (hover: hover) {
     &:hover {
-      background-color: ${({ theme }) => theme.colors.btnGoTopBg};
+      background-color: ${({theme}) => theme.colors.btnGoTopBg};
       transform: translateY(-8px);
     }
   }
